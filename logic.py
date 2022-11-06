9e3c82f8bd2276b63538afb5e49e993953821a0c
MaksymTymeichuk <105876357+MaksymTymeichuk@users.noreply.github.com>
2022-11-06 17:46:56 +0200

    adding some features

--- logic.py
+++ logic.py
@@ -15,6 +15,24 @@ class Search_engine():
         """,self.text2)
         print(self.songs[0])
         return self.songs
+    def select_author_name(self,name):
+        self.album_id = self.db.select("""
+        SELECT AlbumId FROM tracks WHERE Name = ?
+        """,name)
+        self.album = self.db.select("""
+        SELECT Title FROM albums WHERE AlbumId = ?       
+        """,self.album_id[0][0])
+        self.artist_id =self.db.select("""
+        SELECT ArtistId FROM albums WHERE AlbumId = ?
+        """,self.album_id[0][0])
+        self.artist = self.db.select("""
+        SELECT Name FROM artists WHERE ArtistId = ?
+        """,self.artist_id[0][0])
+        self.info_list = []
+        self.info_list.append(self.album)
+        self.info_list.append(self.artist)
+        print(self.info_list[1][0][0])
+        return self.info_list
 """if __name__=="__main__":
    from sql_interface import DbChinook
    db = DbChinook()
--- window.py
+++ window.py
@@ -18,8 +18,21 @@ class Window(Tk):
         self.search_button = Button(self,text="search",width=5,height=2,bg="lightgreen",command = self.check).grid(row=2,column=1)
         self.song_list = Listbox(self,height=25,width=40)
         self.song_list.grid(row=3,column=0)
+        self.song_list.bind('<<ListboxSelect>>', self.selected)
         self.entry1.grid(row=2,column=0,padx=50,pady=10)
-        
+        self.left_lbl = Label(self,text="Тут відобразиться назва пісні",)
+        self.left_lbl.grid(row=1,column=3)
+        self.left_person_lbl = Label(self,text="Тут відобразиться автор пісні")
+        self.left_person_lbl.grid(row=2,column=3)
+        self.left_album_lbl = Label(self,text="Тту відобразиться альбом з якого взята пісня")
+        self.left_album_lbl.grid(row=3,column=3)
+    def selected(self,*args):
+        self.selected_el = self.song_list.get(self.song_list.curselection())
+        self.left_lbl.config(text = "Вибрана пісня: "+self.selected_el)
+        self.info = self.engine.select_author_name(self.selected_el)
+        self.left_person_lbl.config(text = "Автор вибраної пісні: "+self.info[1][0][0])
+        self.left_album_lbl.config(text= "Альбом з якого взято пісню: " + self.info[0][0][0])
+    
     def search_track(self):
         """Пошук треків за назвою треку, або за автором
         або за жанром, в залежності від обраного режиму 
@@ -32,9 +45,6 @@ class Window(Tk):
             self.song_list.insert("end",el[0].strip("{"))
         
     def click_track(self):
-        """Добуває детальну інформацію про обраний 
-        трек
-        """
         pass
 
 
